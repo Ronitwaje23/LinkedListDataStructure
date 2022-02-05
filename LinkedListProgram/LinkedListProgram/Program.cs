660b2d3c82c5955936bc37d51a8768afa3d933ec
ronit#123 <ronitwaje23>
2022-02-05 19:49:25 +0530

    create the linked list

--- LinkedListProgram/LinkedListProgram/Program.cs
+++ LinkedListProgram/LinkedListProgram/Program.cs
@@ -8,10 +8,23 @@ namespace LinkedListProgram
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("welcome to LinkedList program");
-            Console.ReadLine();
+        
+        
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to Linked List");
+
+                LinkedList1 linkedlist = new LinkedList1();
+                linkedlist.Add(56);
+                linkedlist.Add(30);
+                linkedlist.Add(70);
+                linkedlist.Display();
+
+                Console.ReadLine();
+            }
         }
     }
-}
+    
+        
+    
+
